d9daeb44782acedbaf847b01c3fee75f0adb4f2c
trejofran <124050570@upq.edu.mx>
2025-11-03 17:36:06 -0600

    Update IngresosScreen.js

--- AhorraApp/screens/IngresosScreen.js
+++ AhorraApp/screens/IngresosScreen.js
@@ -7,26 +7,143 @@ export default function IngresosScreen() {
     const [cantidad, setCantidad] = useState("");
     const [descripcion, setDescripcion] = useState("");
     return (
-      <View>
-        <Text>Ahorra+ App</Text>
-        <View>
-            <TouchableOpacity>
-                <Text>Ingreso</Text>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Ahorra+ App</Text>
+        <View style={styles.selector}>
+            <TouchableOpacity style={[
+            styles.tab,
+            tipo === "ingreso" && { borderBottomColor: "green" },
+          ]}
+          onPress={() => setTipo("ingreso")}>
+                <Text style={[
+              styles.tabText,
+              { color: tipo === "ingreso" ? "green" : "gray" },
+            ]}>Ingreso</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>Egreso</Text>
+            <TouchableOpacity style={[
+            styles.tab,
+            tipo === "egreso" && { borderBottomColor: "red"},
+            ]}
+          onPress={() => setTipo("egreso")}>
+                <Text style={[
+              styles.tabText,
+              { color: tipo === "egreso" ? "red" : "gray" },
+                ]}>Egreso</Text>
             </TouchableOpacity>
         </View>
-        <View>
-            <Text>
+
+        <View style={styles.form}>
+            <Text style={[
+            styles.label,
+            { color: tipo === "ingreso" ? "green" : "red" },
+          ]}>
                 Nombre Movimiento
             </Text>
             <TextInput
-            
+            style={[
+            styles.input,
+            { borderColor: tipo === "ingreso" ? "green" : "red" },
+            ]}
+            value={nombre}
+            onChangeText={setNombre}
             />
+            <Text style={[
+            styles.label,
+            { color: tipo === "ingreso" ? "green" : "red" },
+          ]}>Categoria</Text>
+            <TextInput
+            style={[
+            styles.input,
+            { borderColor: tipo === "ingreso" ? "green" : "red" },
+          ]}
+            value={categoria}
+            onChangeText={setCategoria}/>
+            <Text style={[
+            styles.label,
+            { color: tipo === "ingreso" ? "green" : "red" },
+          ]}>Cantidad</Text>
+            <TextInput
+            style={[
+            styles.input,
+            { borderColor: tipo === "ingreso" ? "green" : "red" },
+            ]}
+            keyboardType="numeric"
+            value={cantidad}
+            onChangeText={setCantidad}/>
+            <Text style={[
+            styles.label,
+            { color: tipo === "ingreso" ? "green" : "red" },
+          ]}>Descripcion</Text>
+            <TextInput
+             style={[
+            styles.input,
+            { borderColor: tipo === "ingreso" ? "green" : "red" },
+          ]}
+            value={descripcion}
+            onChangeText={setDescripcion}/>
+
+            <TouchableOpacity style={styles.btn}>
+                <Text style={styles.btnText}>Confirmar</Text>
+            </TouchableOpacity>
         </View>
       </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: "#E6EEF8",
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "green",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  selector: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  tab: {
+    paddingVertical: 8,
+    paddingHorizontal: 25,
+    borderBottomWidth: 2,
+    borderBottomColor: "transparent",
+  },
+  tabText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  form: {
+    backgroundColor: "#f4f7fb",
+    padding: 15,
+    borderRadius: 12,
+  },
+  label: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1.5,
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: "white",
+  },
+  btn: {
+    marginTop: 25,
+    backgroundColor: "#2f80ed",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  btnText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
