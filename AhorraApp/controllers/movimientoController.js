796ed48c4499f81a31172869fbbf0925aa15b9e0
Charlyy <124051193@upq.edu.mx>
2025-11-28 20:12:12 -0600

    Arreglo de la base de datos con el seeder

--- AhorraApp/controllers/movimientoController.js
+++ AhorraApp/controllers/movimientoController.js
@@ -1,1 +1,11 @@
 import { Movimiento } from "../models/movimiento";
+import DatabaseService from '../database/db';
+export class MovimientoController{
+    constructor(){
+        this.listeners=[];
+    }
+    async initialize(){
+        await DatabaseService.initialize();
+    }
+
+}
--- AhorraApp/database/db.js
+++ AhorraApp/database/db.js
@@ -1,83 +1,150 @@
 
 import * as SQLite from 'expo-sqlite';
-const db = SQLite.openDatabaseAsync('ahorraApp.db');
-import AsyncStorage from '@react-native-async-storage/async-storage';
-export const getDB=()=>db;
-const databaseService={
-    init:()=>{
-        db.transaction(tx=>{
-            //Para los usuarios
-            tx.executeSql(
-                `CREATE TABLE IF NOT EXISTS usuarios(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nombre TEXT NOT NULL,
-                    email TEXT UNIQUE NOT NULL,
-                    telefono TEXT,
-                    password TEXT NOT NULL
-                );`
-            );
-            tx.executeSql(
-                `CREATE TABLE IF NOT EXISTS categorias(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nombre TEXT NOT NULL UNIQUE,
-                    descripcion TEXT NOT NULL,
-                    presupuesto REAL NOT NULL,
-                    periodo TEXT
-                )`
-            );
-            // constructor(id, descripcion, monto, fecha, tipo, categoria){
-            tx.executeSql(
-                `CREATE TABLE IF NOT EXISTS movimientos(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    descripcion TEXT NOT NULL,
-                    monto REAL NOT NULL,
-                    fecha TEXT NOT NULL,
-                    tipo TEXT NOT NULL,
-                    categoria_id INTEGER NOT NULL,
-                    FOREIGN KEY(categoria_id) REFERENCES categorias(id)
-                );`
-            );
-            
-        })
-    },
-    seed:async()=>{
-        const seeder= await AsyncStorage.getItem('seeder');
-        if(seeder){
-            console.log('La base de datos ya cuenta con datos');
-            return;
-        }
-
-        //Datos para tener en la BD y hacer pruebas
-        db.transaction(tx=>{
-            tx.executeSql(
-                `INSERT INTO usuarios (nombre, email, telefono, password) VALUES (?,?,?,?)`,
-                ['Carlos Mendoza', 'Carlos@gmail.com', '1234567890', '@Caralbmen_0611']
-            );
-            tx.executeSql(
-                `INSERT INTO categorias (nombre, descripcion, presupuesto, periodo)
-                VALUES (?, ?, ?, ?)`,
-                ['Alimentación', 'Gastos de comida y supermercado', 3000, 'Mensual']
-            );
-            tx.executeSql(
-                `INSERT INTO categorias (nombre, descripcion, presupuesto, periodo)
-                VALUES (?, ?, ?, ?)`,
-                ['Transporte', 'Gasolina y transporte público', 1500, 'Mensual']
-            );
-            tx.executeSql(
-                `INSERT INTO movimientos (descripcion, monto, fecha, tipo, categoria_id)
-                VALUES (?, ?, ?, ?, ?)`,
-                ['Compra en supermercado', 500, '2025-11-28', 'Retiro', 1]
-            );
-
-            tx.executeSql(
-                `INSERT INTO movimientos (descripcion, monto, fecha, tipo, categoria_id)
-                VALUES (?, ?, ?, ?, ?)`,
-                ['Pago de gasolina', 800, '2025-11-27', 'Retiro', 2]
-            );
- 
-        });
-        await AsyncStorage.setItem('seeder', 'true');
-        console.log('Seeder Ejecutado');
+class DatabaseService{
+    constructor(){
+        this.db=null;
     }
-};
-export default databaseService;
+    async initialize(){
+        this.db= await SQLite.openDatabaseAsync('AhorraMas.db');
+        await this.db.execAsync('PRAGMA foreign_keys= ON');
+        await this.db.execAsync(`
+            CREATE TABLE IF NOT EXISTS usuarios(
+                id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                telefono TEXT,
+                password TEXT NOT NULL
+            );
+        `);
+        await this.db.execAsync(`
+            CREATE TABLE IF NOT EXISTS categorias(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                descripcion TEXT NOT NULL,
+                presupuesto REAL NOT NULL,
+                periodo TEXT,
+                id_usuario INTEGER NOT NULL,
+                FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario)
+            );
+        `);
+        await this.db.execAsync(`
+            CREATE TABLE IF NOT EXISTS movimientos(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                descripcion TEXT NOT NULL,
+                monto REAL NOT NULL,
+                fecha TEXT NOT NULL,
+                tipo TEXT NOT NULL,
+                categoria_id INTEGER NOT NULL,
+                FOREIGN KEY(categoria_id) REFERENCES categorias(id)
+            );
+        `);
+       
+
+    };
+    async seed(){
+        await this.db.execAsync(`
+            INSERT OR IGNORE INTO usuarios (nombre, email, telefono, password) VALUES (?,?,?,?)`,
+            ['Prueba', 'prueba@gmail.com', '1234567890', 'password']
+        );
+        await this.db.execAsync(
+            `INSERT OR IGNORE INTO categorias (nombre, descripcion, presupuesto, periodo, id_usuario) VALUES (?, ?, ?, ?, ?)`,
+            ['Alimentación', 'Gastos de comida y supermercado', 3000, 'Mensual', 1]
+        );
+        await this.db.execAsync(
+            `INSERT OR IGNORE INTO categorias (nombre, descripcion, presupuesto, periodo, id_usuario) VALUES (?, ?, ?, ?, ?)`,
+            ['Transporte', 'Gasolina y transporte público', 1500, 'Mensual', 1]
+        );
+        await this.db.execAsync(
+            `INSERT OR IGNORE INTO movimientos (descripcion, monto, fecha, tipo, categoria_id) VALUES (?, ?, ?, ?, ?)`,
+            ['Compra en supermercado', 500, '2025-11-28', 'Retiro', 1]
+        );
+        await this.db.execAsync(
+            `INSERT OR IGNORE INTO movimientos (descripcion, monto, fecha, tipo, categoria_id) VALUES (?, ?, ?, ?, ?)`,
+            ['Pago de gasolina', 800, '2025-11-27', 'Retiro', 2]
+        );
+    }
+   
+}
+export default DatabaseService;
+// const db = SQLite.openDatabaseAsync('ahorraApp.db');
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// export const getDB=()=>db;
+// const databaseService={
+//     init:()=>{
+//         db.transaction(tx=>{
+//             //Para los usuarios
+//             tx.executeSql(
+//                 `CREATE TABLE IF NOT EXISTS usuarios(
+//                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//                     nombre TEXT NOT NULL,
+//                     email TEXT UNIQUE NOT NULL,
+//                     telefono TEXT,
+//                     password TEXT NOT NULL
+//                 );`
+//             );
+//             tx.executeSql(
+//                 `CREATE TABLE IF NOT EXISTS categorias(
+//                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//                     nombre TEXT NOT NULL UNIQUE,
+//                     descripcion TEXT NOT NULL,
+//                     presupuesto REAL NOT NULL,
+//                     periodo TEXT
+//                 )`
+//             );
+//             // constructor(id, descripcion, monto, fecha, tipo, categoria){
+//             tx.executeSql(
+//                 `CREATE TABLE IF NOT EXISTS movimientos(
+//                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//                     descripcion TEXT NOT NULL,
+//                     monto REAL NOT NULL,
+//                     fecha TEXT NOT NULL,
+//                     tipo TEXT NOT NULL,
+//                     categoria_id INTEGER NOT NULL,
+//                     FOREIGN KEY(categoria_id) REFERENCES categorias(id)
+//                 );`
+//             );
+            
+//         })
+//     },
+//     seed:async()=>{
+//         const seeder= await AsyncStorage.getItem('seeder');
+//         if(seeder){
+//             console.log('La base de datos ya cuenta con datos');
+//             return;
+//         }
+
+//         //Datos para tener en la BD y hacer pruebas
+//         db.transaction(tx=>{
+//             tx.executeSql(
+//                 `INSERT INTO usuarios (nombre, email, telefono, password) VALUES (?,?,?,?)`,
+//                 ['Carlos Mendoza', 'Carlos@gmail.com', '1234567890', '@Caralbmen_0611']
+//             );
+//             tx.executeSql(
+//                 `INSERT INTO categorias (nombre, descripcion, presupuesto, periodo)
+//                 VALUES (?, ?, ?, ?)`,
+//                 ['Alimentación', 'Gastos de comida y supermercado', 3000, 'Mensual']
+//             );
+//             tx.executeSql(
+//                 `INSERT INTO categorias (nombre, descripcion, presupuesto, periodo)
+//                 VALUES (?, ?, ?, ?)`,
+//                 ['Transporte', 'Gasolina y transporte público', 1500, 'Mensual']
+//             );
+//             tx.executeSql(
+//                 `INSERT INTO movimientos (descripcion, monto, fecha, tipo, categoria_id)
+//                 VALUES (?, ?, ?, ?, ?)`,
+//                 ['Compra en supermercado', 500, '2025-11-28', 'Retiro', 1]
+//             );
+
+//             tx.executeSql(
+//                 `INSERT INTO movimientos (descripcion, monto, fecha, tipo, categoria_id)
+//                 VALUES (?, ?, ?, ?, ?)`,
+//                 ['Pago de gasolina', 800, '2025-11-27', 'Retiro', 2]
+//             );
+ 
+//         });
+//         await AsyncStorage.setItem('seeder', 'true');
+//         console.log('Seeder Ejecutado');
+//     }
+    
+// };
+// export default databaseService;
